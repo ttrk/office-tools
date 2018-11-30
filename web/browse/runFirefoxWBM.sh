7e8ac55d98208896d6aaf7be7433355bf9c44302
Kaya Tatar <tatark@mit.edu>
2018-11-30 10:24:44 +0100

    fix some paths, add new ones

--- web/browse/runFirefoxWBM.sh
+++ web/browse/runFirefoxWBM.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-RUN=327327
+#RUN=327327
+#RUN=327430
+#RUN=327174
+#RUN=327402
+RUN=327495
 
 triggers=(
 # path name, ID
+"L1_ZeroBias,107"
 "L1_MinimumBiasHF1_AND_BptxAND,154"
-"L1_MinimumBiasHF2_AND_BptxAND,156"
+"L1_MinimumBiasHF1_OR_BptxAND,155"
+"L1_MinimumBiasHF2_AND_BptxAND,164"
+"L1_MinimumBiasHF2_OR_BptxAND,164"
 "L1_SingleEG7_BptxAND,346"
 "L1_SingleEG21_BptxAND,349"
 "L1_SingleEG30_BptxAND,350"
@@ -17,6 +24,14 @@ triggers=(
 "HLT_HIGEDPhoton30_EB_v1,2134258"          # menu v1.5 : 2133569
 "HLT_HIIslandPhoton30_Eta2p4_v1,2134242"   # menu v1.5 : 2133553
 "HLT_HIIslandPhoton30_Eta1p5_v1,2134243"   # menu v1.5 : 2133554
+#"HLT_HIGEDPhoton10_v1,2134640"              # menu v1.5 : 2133893
+#"HLT_HIGEDPhoton10_EB_v1,2134256"           # menu v1.5 : 2133567
+#"HLT_HIIslandPhoton10_Eta2p4_v1,2134639"    # menu v1.5 : 2133892
+#"HLT_HIIslandPhoton10_Eta1p5_v1,2134239"    # menu v1.5 : 2133550
+#"HLT_HIGEDPhoton20_v1,2134251"              # menu v1.5 : 2133562
+#"HLT_HIGEDPhoton20_EB_v1,2134257"           # menu v1.5 : 2133568
+#"HLT_HIIslandPhoton20_Eta2p4_v1,2134240"    # menu v1.5 : 2133551
+#"HLT_HIIslandPhoton20_Eta1p5_v1,2134241"    # menu v1.5 : 2133552
 );
 
 linkL1Temp="https://cmswbm.cern.ch/cmsdb/servlet/ChartL1TriggerRates?fromTime=&toTime=&fromLSNumber=&toLSNumber=&minRate=&maxRate=&minCount=&maxCount=&beforePrescale=1&rates=1&postDeadRatesHLT=1&drawCounts=0&drawLumisec=1&runID=YOURRUN&bitID=YOURPATHID&type=0&TRIGGER_NAME=YOURPATHNAME&LSLength=23.31040958"
